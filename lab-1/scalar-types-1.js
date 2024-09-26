438e6026a41d081355f3b2e0c4e594cc5cf28293
Daria Kroshka <146644868+luvremak@users.noreply.github.com>
2024-09-26 23:10:34 +0300

    Update scalar-types-1.js

--- lab-1/scalar-types-1.js
+++ lab-1/scalar-types-1.js
@@ -2,7 +2,7 @@ let a = 5;
 const b = inc(a);
 
 function inc(number) {
-    number = (2 * number * number) - 10 * number + 1;
+    number = Math.floor(Math. sqrt((2 * number * number) - 9 * (number + 1) + 20 * number - 1));
     return number;
 }
 
