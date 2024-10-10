4808c99abe8518cecc7bb274038b00db20c30b28
Daria Kroshka <146644868+luvremak@users.noreply.github.com>
2024-10-10 22:43:11 +0300

    Update data-types-2.js

--- lab-1/data-types-2.js
+++ lab-1/data-types-2.js
@@ -1,27 +1,17 @@
 let arr = [true, 'hello', 5, 12, -200, false, false, 'word', 7, 9, false,'true', false, true, -11, 'string', 'text', 'word', 17, true,'seven'];
-let obj = {};
+
+let obj = {
+    number: 0,
+    string: 0,
+    boolean: 0,
+};
 
 function inc() {
     for (let value of arr) {
-        if (typeof value === 'number') {
-            if (!obj['number']) {
-                obj['number'] = 0;
-            }
-            obj['number'] += 1;
-        }
-        if (typeof value === 'string') {
-            if (!obj['string']){
-            obj['string'] = 0;
-        }
-            obj['string'] += 1;
-        }
-        if (typeof value === 'boolean') {
-            if (!obj['boolean']){
-            obj['boolean'] = 0;
-        }
-            obj['boolean'] += 1;
-        }
+        obj[typeof value]++;
     }
 }
+
 inc();
 console.log(obj);
+
