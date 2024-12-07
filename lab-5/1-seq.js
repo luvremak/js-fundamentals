a95c6f2426eb60378820f179314657639d513fb1
Daria Kroshka <146644868+luvremak@users.noreply.github.com>
2024-12-07 21:55:15 +0200

    Update 1-seq.js

--- lab-5/1-seq.js
+++ lab-5/1-seq.js
@@ -1,1 +1,15 @@
+'use strict';
 
+const seq = (f) => (g) =>
+  typeof g === 'number'
+    ? f(g) 
+    : seq((x) => f(g(x))); 
+
+console.log(
+  seq((x) => x + 1)
+    ((x) => x * 2)
+    ((x) => x / 3)
+    ((x) => x - 4)(7)
+);
+
+module.exports = { seq };
