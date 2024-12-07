49987ed2992a6a1edbb5b7a97e7fdb4eb63770d4
Daria Kroshka <146644868+luvremak@users.noreply.github.com>
2024-12-07 22:55:18 +0200

    Update 2-array.js

--- lab-5/2-array.js
+++ lab-5/2-array.js
@@ -1,1 +1,40 @@
+'use strict';
+
+const array = () => {
+  const data = [];
+
+  const get = (i) => {
+    return data[i];
+  };
+
+  const push = (x) => {
+    data.push(x);
+  };
+
+  const pop = () => {
+    return data.pop();
+  };
+
+  return {
+    get,
+    push,
+    pop,
+  };
+};
+
+const arr = array();
+
+arr.push('first');
+arr.push('second');
+arr.push('third');
+
+console.log(arr.get(0)); 
+console.log(arr.get(1)); 
+console.log(arr.get(2)); 
+
+console.log(arr.pop()); 
+console.log(arr.pop()); 
+console.log(arr.pop()); 
+
+console.log(arr.pop()); 
 
